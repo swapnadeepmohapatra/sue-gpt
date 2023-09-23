ee5018b796e0b53ea0c2398ff231aa0be141f448
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2023-09-23 14:13:00 +0530

    [Added] basic landing page

--- app/page.js
+++ app/page.js
@@ -1,95 +1,61 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <h1
+          style={{
+            fontSize: "2rem",
+          }}
+        >
+          SueGPT
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            gap: "1rem",
+          }}
+        >
+          <p>Document Generation </p>
+          <p> Legal Advice</p>
+          <p> Who we are</p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h1 style={{ textAlign: "center" }} className={styles.center}>
+        #BeTheLegalForce <br /> #BeTheChange
+      </h1>
+      <button className={styles.button}>
+        <div>Newsfeed</div>
+      </button>
+      <h1
+        style={{
+          alignSelf: "self-start",
+          fontSize: "1.5rem",
+        }}
+      >
+        Our Mission
+      </h1>
+      <p
+        style={{
+          textAlign: "justify",
+        }}
+      >
+        SueGPT is not only a SIH project but a forecoming revolution in the
+        legal sector of India. We always hear about the daily legal injustices
+        and how hard it is to afford a lawyer or advocate for middle class and
+        rural people….and how to cope up with the nuances of a legal document
+        and to ensure its legibility? Consider it's done.
+        <br />
+        We at SueGPT believe that it is high time to integrate the magic of NLP
+        efficiently in our legal system. We cater services like legal advice and
+        legal notice and agreement pdf generation and legibility checking using
+        state-of-the-art ChatGPT 3.5 Turbo LLM to general public, legal
+        professionals and corporate firms. With all your support, we can take
+        SueGPT to greater heights and fill the void which our legal system is
+        going through. <strong>#BeTheLegalForce #BeTheChange</strong>
+      </p>
     </main>
-  )
+  );
 }
